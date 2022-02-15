e71b19c25bde60faf627adb0c8d35f01c7c807af
jeremiahlee94 <jhwlee94@gmail.com>
2022-02-15 13:54:29 -0800

    remove user views

--- users/views.py
+++ users/views.py
@@ -1,19 +1,3 @@
 from django.shortcuts import render
-# from rest_framework import generics, permissions
-# from .serializers import UserCreateSerializer
-# from .models import User
-# from expense_tracker.permissions import IsOwnerOrReadOnly
 
 # # Create your views here.
-# class UserList(generics.ListCreateAPIView):
-#     queryset = User.objects.all()
-#     serializer_class = UserCreateSerializer
-#     permission_classes = [permissions.IsAuthenticated]
-
-#     def get_queryset(self):
-#         """
-#         This view should return a list of all the purchases
-#         for the currently authenticated user.
-#         """
-#         user = self.request.user
-#         return User.objects.filter(username=user)
